8b8b6f68e790f0127ca1b3b51c39e84356968694
GreyA <grey.altaer@gmail.com>
2025-03-10 11:32:00 -0700

    mvp

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import PokemonList from "./components/PokemonList/PokemonList";
 import NavBar from "./components/Navbar/Navbar.jsx";
 import PokemonDetails from "./components/PokemonDetails/PokemonDetails.jsx";
+import PokemonForm from "./components/PokemonForm/PokemonForm.jsx";
 
 const initialState = [
   { _id: 1, name: "bulbasaur", weight: 69, height: 7 },
@@ -14,12 +15,19 @@ const initialState = [
 
 const App = () => {
   const [pokemon, setPokemon] = useState(initialState);
+
+  const addPokemon = (newPokemonData) => {
+    newPokemonData._id = pokemon.length +1;
+    setPokemon([ ...pokemon, newPokemonData ]);
+  };
+
   return (
     <>
       <NavBar />
       <Routes>
         <Route path="/" element={<h2>Home Page</h2>} />
         <Route  path="/pokemon" element={ <PokemonList pokemon={ pokemon } /> } />
+        <Route path="/pokemon/new" element={ <PokemonForm addPokemon={addPokemon} /> }/>
         <Route
           path="pokemon/:pokemonId"
           element={ <PokemonDetails pokemon={ pokemon } /> }
